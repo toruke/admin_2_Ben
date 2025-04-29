53ff37d64e4d065f5116289333879b438194dc9e
root <root@vps-33a30cda.vps.ovh.net>
2025-04-29 22:16:15 +0000

    modification ajoue d'un certificat et test avec let's encrypt

--- casino/web/woodytoys.sql
+++ casino/web/woodytoys.sql
@@ -4,6 +4,6 @@ CREATE TABLE products (id mediumint(8) unsigned NOT NULL auto_increment,product_
 
 INSERT INTO products (product_name,product_price) VALUES ("Set de 100 cubes multicolores","50"),("Yoyo","10"),("Circuit de billes","75"),("Arc à flèches","20"),("Maison de poupées","150");
 
-CREATE USER IF NOT EXISTS 'woodytoys'@'%' IDENTIFIED BY 'mot_de_passe_securise';
+CREATE USER IF NOT EXISTS 'woodytoys'@'api' IDENTIFIED BY 'mypass';
 GRANT ALL PRIVILEGES ON woodytoys.* TO 'woodytoys'@'%';
 FLUSH PRIVILEGES;
